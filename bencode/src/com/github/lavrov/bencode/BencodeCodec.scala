06b0a8ad8c1e9492e49e1c16d33d3e96c9c2261e
rob <rob@messagebird.cojm>
2019-06-23 17:37:02 +0200

    solution to a workshop excersise

--- bencode/src/com/github/lavrov/bencode/BencodeCodec.scala
+++ bencode/src/com/github/lavrov/bencode/BencodeCodec.scala
@@ -1,19 +1,87 @@
 package com.github.lavrov.bencode
 
-import scodec.Codec
-import scodec.codecs._
-import scodec.Attempt
-import scodec.Err
-import scodec.SizeBound
-import scodec.DecodeResult
+import scodec.Attempt.{Failure, Successful}
 import scodec.bits.BitVector
-import scodec.Encoder
-import scodec.Decoder
-import scodec.bits.ByteVector
+import scodec.codecs._
+import scodec.{Attempt, Codec, DecodeResult, Decoder, Encoder, Err, SizeBound}
+
+import scala.util.Try
+
 
 object BencodeCodec {
-  val instance: Codec[Bencode] = {
-    lazily(???) // Implement codec!
+  val instance: Codec[Bencode] = lazily {
+    choice(bencodeString.upcast, bencodeInteger.upcast, bencodeList.upcast, bencodeDict.upcast)
+  }
+  
+  object AsciiNumber {
+    
+    private val positiveAsciiDigit: Codec[Short] = {
+      def decode(b: Byte): Attempt[Short] = {
+        Try(Character.toString(b.toChar).toShort) match {
+          case scala.util.Success(value) => Successful(value)
+          case scala.util.Failure(_) => Failure(Err("Can not decode"))
+        }
+      }
+      def encode(c: Short) =
+        Successful(c.toByte)
+      
+      byte.exmap(decode, encode)
+    }
+    
+    val positiveAsciiNumber: Codec[Long] = {
+      def listToLong(ds: List[Short]): Long = ds.reverse.zipWithIndex.foldLeft(0l){
+        case (acc, (d, i)) => (d * scala.math.pow(10, i)).toLong + acc
+      }
+      
+      def longToList(d: Long): List[Short] = d.toString.toList.map(_.toShort)
+      
+      listSuccessful(positiveAsciiDigit).xmap[Long](listToLong, longToList)
+    }
+    
+    val asciiNumber: Codec[Long] = {
+      recover(constant('-')).consume{
+        case true => positiveAsciiNumber.xmapc(d => -d)(d => -d)
+        case false => positiveAsciiNumber
+      }(d => d < 0)
+    }
+  }
+  
+  
+  def bencodeInteger: Codec[Bencode.BInteger] = {
+    val bci = AsciiNumber.asciiNumber.xmapc(d => Bencode.BInteger(d))(bd => bd.value)
+    constant('i') ~> bci <~ constant('e')
+  }
+  
+  def bencodeString: Codec[Bencode.BString] = {
+    val stringLength = AsciiNumber.positiveAsciiNumber <~ constant(':')
+    val str = stringLength.consume{ len =>
+      bytes(len.toInt)
+    }(s => s.length)
+    str.xmapc(Bencode.BString(_))(_.value)
+  }
+  
+  def bencodeList: Codec[Bencode.BList] = {
+    val bcl = constant('l') ~> listSuccessful(instance) <~ constant('e')
+    bcl.xmap(Bencode.BList, _.values)
+  }
+  
+  def bencodeDict: Codec[Bencode.BDictionary] = {
+    def bcTuple: Codec[(String, Bencode)] =
+      (bencodeString ~ instance).xmapc{
+        case (bs, v) => bs.value.decodeAscii match {
+          case Right(value) => value -> v
+          case Left(err) => throw err
+        }
+      }{
+        case (s, v) => Bencode.BString(s) -> v
+      }
+    
+    def bcDict: Codec[Bencode.BDictionary] = {
+      val bcd = constant('d') ~> listSuccessful(bcTuple) <~ constant('e')
+      bcd.xmapc(Bencode.BDictionary(_:_*))(_.values.toList)
+    }
+    
+    bcDict
   }
 
   /**
